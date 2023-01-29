177cd3babf1fa54a6abe98d4b4d939dafa9fe52c
hyonie <dev.hyonie@gmail.com>
2023-01-30 06:15:37 +0900

    feat: BooksCard API

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -5,11 +5,13 @@ export default function BookCard({ result }) {
   return (
     <div>
       {/* <img width={130} src={result.cover}></img> */}
-      <img className="card" src={result.cover}></img>
-
       {/* <div>{item.cover}</div>
       <h6>{item.title}</h6>
       <div>{item.author}</div> */}
+      <img className="card" src={result.cover}></img>
+      <div>
+        <h5>{result.title}</h5>
+      </div>
     </div>
   );
 }
