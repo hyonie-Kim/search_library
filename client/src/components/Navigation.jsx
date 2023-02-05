5162180a7c660c97ebbd29c023e7305f79f36c9f
hyonie <dev.hyonie@gmail.com>
2023-02-05 19:39:03 +0900

    feat: navbar 수정

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -32,49 +32,6 @@ export default function Navigation() {
   };
   return (
     <>
-      {/* <div>
-        <FontAwesomeIcon icon={faUser} />
-        <div>로그인</div>
-      </div> */}
-      {/* <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">
-            <img
-              width={50}
-              src="https://sesac.seoul.kr/static/common/images/www/temp/thumbnail.png"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Link to="/" className="nav-item">
-                Home
-              </Link>
-              <Link to="/books" className="nav-item">
-                Books
-              </Link>
-
-              <Nav.Link href="/login" className="justify-content-end">
-                <FontAwesomeIcon icon={faUser} />
-                로그인
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar> */}
       <Navbar bg="light" expand="lg">
         <Container fluid>
           <Navbar.Brand href="#">
