82ea529f6a6a4d5e49f00d4840a8a7b94e7a9637
hyonie <dev.hyonie@gmail.com>
2023-02-05 20:09:14 +0900

    fear: navigation style

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -10,6 +10,7 @@ import {
   Nav,
   NavDropdown,
   FormControl,
+  input,
 } from "react-bootstrap";
 
 import { Input } from "antd";
@@ -32,7 +33,7 @@ export default function Navigation() {
   };
   return (
     <>
-      <Navbar bg="light" expand="lg">
+      {/* <Navbar bg="light" expand="lg">
         <Container fluid>
           <Navbar.Brand href="#">
             <img
@@ -53,8 +54,10 @@ export default function Navigation() {
             <Nav>
               <Nav.Link onClick={goToLogin}>
                 <FontAwesomeIcon icon={faUser} /> {""} 로그인
-              </Nav.Link>
-              {/* <NavDropdown
+              </Nav.Link> */}
+
+      {/* ddddddddddddddddddddddddddddddddddddddddd */}
+      {/* <NavDropdown
                 title={
                   <span>
                     <FontAwesomeIcon icon={faUser} />
@@ -72,8 +75,9 @@ export default function Navigation() {
                   Something else here
                 </NavDropdown.Item>
               </NavDropdown> */}
-            </Nav>
-            {/* <Form className="d-flex">
+      {/* </Nav> */}
+
+      {/* <Form className="d-flex">
               <Form.Control
                 type="search"
                 placeholder="Search"
@@ -82,14 +86,94 @@ export default function Navigation() {
               />
               <Button variant="outline-success">Search</Button>
             </Form> */}
-            <Search
+      {/* <Search
               placeholder="input search text"
               onSearch={(value) => searchOnkeyPress(value)}
               style={{ width: 200 }}
             />
           </Navbar.Collapse>
         </Container>
-      </Navbar>
+      </Navbar> */}
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor01"
+            aria-controls="navbarColor01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Home
+                  <span class="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  href="#"
+                  role="button"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </div>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input
+                class="form-control me-sm-2"
+                type="search"
+                placeholder="Search"
+              />
+              <button class="btn btn-secondary my-2 my-sm-0" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
