471b5fe64df18c8d55f90c5b666dda43b094a345
hyonie <dev.hyonie@gmail.com>
2023-03-07 19:02:02 +0900

    feat: MySql 로컬

--- server/config/config.js
+++ server/config/config.js
@@ -8,4 +8,13 @@ const development = {
   dialect: "mysql",
 };
 
+const production = {
+  host: env.MYSQL_HOST,
+  database: env.MYSQL_DATABASE,
+  username: env.MYSQL_USERNAME,
+  password: env.MYSQL_PASSWORD,
+  dialect: "mysql",
+  //port: env.MYSQL_PORT
+};
+
 module.exports = { development };
